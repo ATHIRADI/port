b752fc7bb52ee1145019ee4a75a6de45b6b81396
ATHIRADI ARUN <80013677+ATHIRADI@users.noreply.github.com>
2023-02-19 03:21:12 +0530

    Update About.jsx

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -13,7 +13,7 @@ export const About = () => {
                 <div className="w-full h-full px-5 my-5 space-y-10 lg:px-28">
                     <h1 className="w-40 py-2 my-10 text-xl font-bold uppercase border-b-8 text-start border-black-color">ABOUT</h1>
                     <hr className="h-px border-0 bg-primary-color " />
-                    <Image className="h-[350px] object-cover  w-full  lg:h-[650px] ring-secoundary-color rounded-md " src={aboutContent.aboutImage} width="1000" height="1000" />
+                    <Image className="h-[350px] object-cover  w-full  lg:h-[650px] ring-secoundary-color rounded-md " src={aboutContent.aboutImage} alt="aboutImage" width="1000" height="1000" />
                     <h1 className="text-4xl font-bold uppercase">{aboutContent.name}</h1>
                     <TypeEffect />
                     <Intro />
