6a37b6d40a6e304ebffd3511477878388cf11138
ATHIRADI ARUN <80013677+ATHIRADI@users.noreply.github.com>
2023-02-19 03:22:01 +0530

    Update Hero.jsx

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
         <section id="hero" className="relative bg-primary-color">
             <div className="container relative z-30 flex flex-col items-center justify-center max-w-6xl min-h-screen gap-5 px-5 mx-auto lg:flex-row lg:px-28">
                 <div className="w-full h-full my-5 mt-16 lg:mt-0">
-                    <Image className="h-[300px] object-cover m-auto w-[300px] lg:w-[400px] lg:h-[400px] ring-secoundary-color rounded-md ring-8 shadow-2xl" src={heroContent.heroImage} width="1000" height="1000" />
+                    <Image className="h-[300px] object-cover m-auto w-[300px] lg:w-[400px] lg:h-[400px] ring-secoundary-color rounded-md ring-8 shadow-2xl" alt="heroImage" src={heroContent.heroImage} width="1000" height="1000" />
                 </div>
                 <div className="flex flex-col items-center justify-center w-full h-full my-5 space-y-5 text-black-color lg:items-start">
                     <h1 className="text-4xl font-bold uppercase">{heroContent.name}</h1>
